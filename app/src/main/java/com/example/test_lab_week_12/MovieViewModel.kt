fba7af93d4731ed7e03045284083819b40567261
P0Y0Y0 <erufumang@gmail.com>
2025-12-03 09:32:05 +0700

    Commit No. 4 – Assignment

--- app/src/main/java/com/example/test_lab_week_12/MovieViewModel.kt
+++ app/src/main/java/com/example/test_lab_week_12/MovieViewModel.kt
@@ -8,9 +8,9 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.launch
+import java.util.Calendar
 
 class MovieViewModel(private val movieRepository: MovieRepository) : ViewModel() {
-
     private val _popularMovies = MutableStateFlow(emptyList<Movie>())
     val popularMovies: StateFlow<List<Movie>> = _popularMovies
 
@@ -23,9 +23,19 @@ class MovieViewModel(private val movieRepository: MovieRepository) : ViewModel()
 
     private fun fetchPopularMovies() {
         viewModelScope.launch(Dispatchers.IO) {
+
+            val currentYear = Calendar.getInstance().get(Calendar.YEAR).toString()
+
             movieRepository.fetchMovies()
-                .catch { e -> _error.value = "An exception occurred: ${e.message}" }
-                .collect { movies -> _popularMovies.value = movies }
+                .catch { e ->
+                    _error.value = "An exception occurred: ${e.message}"
+                }
+                .collect { movies ->
+                    val filtered = movies
+                        .filter { it.releaseDate?.startsWith(currentYear) == true }
+
+                    _popularMovies.value = filtered
+                }
         }
     }
 }
